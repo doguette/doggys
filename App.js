527112573306e6ed496dfcde6a7529df2cbe4201
doguette <mrrobino2000@gmail.com>
2023-12-18 00:09:08 -0600

    punki super fiesta

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Este es el cambio 1</Text>
+      <Text style={styles.text}>Este es el cambio 2</Text>
       <StatusBar style="auto" />
     </View>
   );
